1965e96b34f97458120edd1c2cc9c11c4c7ebddb
yurikrupnik <krupnik.yuri@gmail.com>
2018-08-24 02:43:02 +0300

    webpack build and prepublish

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    context: path.resolve(__dirname, 'app'),
+    target: 'node', // in order to ignore built-in modules like path, fs, etc.
+    node: {
+        __dirname: false,
+        __filename: true,
+    },
+    // externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+    devtool: 'source-map',
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        chunkFilename: '[name].js'
+    },
+    mode: 'development', // production
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: ['babel-loader', 'eslint-loader'],
+            }
+        ]
+    }
+};
