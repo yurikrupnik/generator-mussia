bb9700ae35faf333b27297e0540b08b280788beb
yurikrupnik <krupnik.yuri@gmail.com>
2019-03-11 14:44:29 +0200

    updated client using config for render type

--- app/index.js
+++ app/index.js
@@ -266,6 +266,7 @@ class App extends Generator {
             'react-router',
             'react-router-dom'
         ]);
+        this.npmInstall();
     }
 
     end() {
--- app/templates/client.jsx
+++ app/templates/client.jsx
@@ -3,10 +3,11 @@ import { render<%= filters.render ? ', hydrate ' : ' ' %>} from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import routes from './components/routes';
+import config from './config';
 import './styles/_index.scss';
 
 <%_ if (filters.render) { _%>
-const renderMethod = module.hot ? render : hydrate;
+const renderMethod = config.isProd ? hydrate : render
 
 <%_ } _%>
 <%=  filters.render ? 'renderMethod' : 'render'%>(
